2d9ef4f079f9f314ff455a69b6eb7842bf87b2e2
RTH <43406476+RoadToHacker@users.noreply.github.com>
2018-09-26 14:27:29 +0200

    Create style.py

---
+++ modules/style.py
@@ -0,0 +1,34 @@
+
+# Colors Formatting
+
+HEADER = '\033[95m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+WHITE = '\033[97m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BRIGHT = '\033[1m'
+EBRIGHT = '\033[0m'
+
+
+banner = '''%s
+                  (
+ %sCubedoooooooor!%s
+              (   ()   )
+    ) ________    //  )
+ ()  |\\       \\  //
+( \\\__ \\ ______\\//
+   \\__) |       | %s*%s Cubedoor Malware Generator%s 
+     |  |       | %s*%s Coded by RoadToHacker%s
+      \\ |       | %s*%s Instagram: road.to.hacker%s
+       \\|_______| %s*%s Github: https://github.com/RoadToHacker%s
+       //    \\ 
+      ((     ||  %sDevelopers assume no liability and are not responsable%s 
+       \\\\    ||   %sfor any misuse or damage caused by this program.%s
+     ( ()    ||
+      (      () ) )
+
+''' %(RED, WHITE, RED, GREEN, WHITE, RED, GREEN, WHITE, RED, GREEN, WHITE, RED, GREEN, WHITE, RED,
+	WHITE, RED, WHITE, RED)
+
